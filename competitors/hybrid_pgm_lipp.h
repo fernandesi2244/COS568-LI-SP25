1e1b3da2ec79aab3300ff1b86f1aa62ca35de58d
Ian Fernandes <fernandesi2244@gmail.com>
2025-05-05 20:50:57 -0400

    make flush async

--- competitors/hybrid_pgm_lipp.h
+++ competitors/hybrid_pgm_lipp.h
@@ -3,82 +3,75 @@
 #ifndef TLI_HYBRID_PGM_LIPP_H
 #define TLI_HYBRID_PGM_LIPP_H
 
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
 #include <vector>
-#include <memory>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
 
 #include "../util.h"
 #include "base.h"
 #include "dynamic_pgm_index.h"
 #include "lipp.h"
 
-template <class KeyType, class SearchClass, size_t pgm_error = 64> //, size_t flush_threshold_percent = 5>
+template <class KeyType, class SearchClass, size_t pgm_error = 64>
 class HybridPGMLIPP : public Competitor<KeyType, SearchClass> {
  public:
-  HybridPGMLIPP(const std::vector<int>& params = std::vector<int>())
-      : lipp_(params), pgm_(params), pgm_size_(0), flush_count_(0)
-  {
-    // Use the template parameter as default, but allow override from params
-    flush_threshold_ = params.size() > 0 ? params[0] : 5; // 5 is default flush threshold percentage
+  HybridPGMLIPP(const std::vector<int>& params = {})
+      : lipp_(params),
+        pgm_(params),
+        pgm_size_(0),
+        flush_threshold_percent_(params.empty() ? 5 : params[0]),
+        stop_flush_(false),
+        flush_in_progress_(false) {
+    flush_thread_ = std::thread(&HybridPGMLIPP::AsyncFlushWorker, this);
   }
 
-  uint64_t Build(const std::vector<KeyValue<KeyType>>& data, size_t num_threads) {
-    // Save initial data size to calculate threshold
-    initial_data_size_ = data.size();
-    flush_threshold_count_ = initial_data_size_ * flush_threshold_ / 100 / 10;
+  ~HybridPGMLIPP() {
+    stop_flush_ = true;
+    cv_flush_.notify_all();
+    if (flush_thread_.joinable()) flush_thread_.join();
+  }
 
+  uint64_t Build(const std::vector<KeyValue<KeyType>>& data, size_t num_threads) override {
+    initial_data_size_ = data.size();
+    flush_threshold_count_ = initial_data_size_ * flush_threshold_percent_ / 100 / 10;
     return lipp_.Build(data, num_threads);
   }
 
-  size_t EqualityLookup(const KeyType& lookup_key, uint32_t thread_id) const {
-    // First try to find in PGM (where newer data is)
-    size_t pgm_res = pgm_.EqualityLookup(lookup_key, thread_id);
-    if (pgm_res != util::NOT_FOUND) {
-      return pgm_res;
+  size_t EqualityLookup(const KeyType& key, uint32_t thread_id) const override {
+    size_t res = pgm_.EqualityLookup(key, thread_id);
+    return (res != util::NOT_FOUND) ? res : lipp_.EqualityLookup(key, thread_id);
+  }
+
+  uint64_t RangeQuery(const KeyType& lower, const KeyType& upper, uint32_t thread_id) const override {
+    return pgm_.RangeQuery(lower, upper, thread_id) + lipp_.RangeQuery(lower, upper, thread_id);
+  }
+
+  void Insert(const KeyValue<KeyType>& data, uint32_t thread_id) override {
+    {
+      std::lock_guard<std::mutex> lock(pgm_mutex_);
+      pgm_data_.emplace_back(data);
+      pgm_.Insert(data, thread_id);
+      pgm_size_++;
     }
 
-    // If not found in PGM, check LIPP
-    return lipp_.EqualityLookup(lookup_key, thread_id);
+    if (pgm_size_ >= flush_threshold_count_ && !flush_in_progress_) {
+      flush_in_progress_ = true;
+      cv_flush_.notify_one();
+    }
   }
 
-  uint64_t RangeQuery(const KeyType& lower_key, const KeyType& upper_key, uint32_t thread_id) const {
-    // Aggregate across both PGM and LIPP
-    uint64_t pgm_res = pgm_.RangeQuery(lower_key, upper_key, thread_id);
-    uint64_t lipp_res = lipp_.RangeQuery(lower_key, upper_key, thread_id);
+  std::string name() const override { return "HybridPGMLIPP"; }
 
-    return pgm_res + lipp_res;
+  std::size_t size() const override { return pgm_.size() + lipp_.size(); }
+
+  bool applicable(bool unique, bool range_query, bool insert, bool multithread, const std::string&) const override {
+    return unique && !multithread && SearchClass::name() != "LinearAVX";
   }
 
-  void Insert(const KeyValue<KeyType>& data, uint32_t thread_id) {
-    // Insert into PGM
-    pgm_data_.emplace_back(data);
-    pgm_.Insert(data, thread_id);
-
-    pgm_size_++;
-
-    // Check if we need to flush from PGM to LIPP
-    FlushIfNeeded(thread_id);
-  }
-
-  std::string name() const { return "HybridPGMLIPP"; }
-
-  std::size_t size() const { return pgm_.size() + lipp_.size(); }
-
-  bool applicable(bool unique, bool range_query, bool insert, bool multithread, const std::string& ops_filename) const {
-    // Both LIPP and PGM have unique key requirements
-    std::string name = SearchClass::name();
-    return name != "LinearAVX" && unique && !multithread;
-  }
-
-  std::vector<std::string> variants() const { 
-    std::vector<std::string> vec;
-    vec.push_back(SearchClass::name());
-    vec.push_back(std::to_string(pgm_error));
-    vec.push_back(std::to_string(flush_threshold_));
-    vec.push_back(std::to_string(flush_count_)); // Added flush count to variants
-    return vec;
+  std::vector<std::string> variants() const override {
+    return {SearchClass::name(), std::to_string(pgm_error), std::to_string(flush_threshold_percent_)};
   }
 
  private:
@@ -86,34 +79,41 @@ class HybridPGMLIPP : public Competitor<KeyType, SearchClass> {
   DynamicPGM<KeyType, SearchClass, pgm_error> pgm_;
 
   std::vector<KeyValue<KeyType>> pgm_data_;
-  
-  // Threshold parameters
+  mutable std::mutex pgm_mutex_;
+
   size_t initial_data_size_;
-  size_t flush_threshold_; // Percentage of initial data size
-  size_t flush_threshold_count_; // Absolute count for flushing
-  size_t pgm_size_ = 0; // Current size of PGM data
-  
-  // Flush counter
-  size_t flush_count_; // Added counter to track number of flushes
+  size_t flush_threshold_percent_;
+  size_t flush_threshold_count_;
+  std::atomic<size_t> pgm_size_;
 
-  // Flush data from PGM to LIPP if needed
-  void FlushIfNeeded(uint32_t thread_id) {
-    if (pgm_size_ < flush_threshold_count_) {
-      return; // No need to flush yet
+  std::thread flush_thread_;
+  mutable std::mutex flush_mutex_;
+  std::condition_variable cv_flush_;
+  std::atomic<bool> stop_flush_;
+  std::atomic<bool> flush_in_progress_;
+
+  void AsyncFlushWorker() {
+    while (!stop_flush_) {
+      std::unique_lock<std::mutex> lock(flush_mutex_);
+      cv_flush_.wait(lock, [this]() { return flush_in_progress_ || stop_flush_; });
+
+      if (stop_flush_) break;
+
+      std::vector<KeyValue<KeyType>> local_data;
+      {
+        std::lock_guard<std::mutex> pgm_lock(pgm_mutex_);
+        local_data.swap(pgm_data_);
+        pgm_size_ = 0;
+        pgm_ = DynamicPGM<KeyType, SearchClass, pgm_error>(); // reset PGM
+      }
+
+      if (!local_data.empty()) {
+        lipp_.BulkInsert(local_data, 0);
+      }
+
+      flush_in_progress_ = false;
     }
-
-    // Increment flush counter
-    flush_count_++;
-
-    // Move data from PGM to LIPP
-    for (const auto& data : pgm_data_) {
-      lipp_.Insert(data, thread_id);
-    }
-    pgm_data_.clear(); // Clear PGM data after moving to LIPP
-    pgm_size_ = 0; // Reset PGM size
-    pgm_ = DynamicPGM<KeyType, SearchClass, pgm_error>(std::vector<int>()); // Reset PGM
-    std::cout << "Flushed " << flush_threshold_count_ << " items from PGM to LIPP. Flush count: " << flush_count_ << std::endl;
   }
 };
 
-#endif  // TLI_HYBRID_PGM_LIPP_H
+#endif
